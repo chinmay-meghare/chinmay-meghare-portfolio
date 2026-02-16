162ce8aaec2d274022c1dc7e4e767a9df4b205f2
chinmay-meghare <chinmaym100@gmail.com>
2026-02-16 23:11:13 +0530

    chore: add basic routes, folder & pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 function App() {
-
-
   return (
     <>
       <div>
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
