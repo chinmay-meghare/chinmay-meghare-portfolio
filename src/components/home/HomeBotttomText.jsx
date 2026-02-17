2fb1f2bfaadce6d44722d3d570da7d0677034808
chinmay-meghare <chinmaym100@gmail.com>
2026-02-17 19:48:10 +0530

    feat(home): complete Home page

--- src/components/home/HomeBotttomText.jsx
+++ src/components/home/HomeBotttomText.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const HomeBotttomText = () => {
   return (
-    <div>HomeBotttomText</div>
-  )
-}
+    <div className="font-[font-2] flex items-center justify-center gap-2">
+      <Link
+        className="text-[6vw] leading-[4.9rem] hover:border-[#D3FD50] hover:text-[#D3FD50] border-5 border-white rounded-full px-8 uppercase"
+        to="/projects"
+      >
+        Projects
+      </Link>
+      <Link
+        className="text-[6vw] leading-[4.9rem] hover:border-[#D3FD50] hover:text-[#D3FD50] border-5 border-white rounded-full px-8 uppercase"
+        to="/about"
+      >
+        About
+      </Link>
+    </div>
+  );
+};
 
-export default HomeBotttomText
+export default HomeBotttomText;
--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -4,9 +4,9 @@ import Video from "./Video";
 const HomeHeroText = () => {
   return (
     <div className="font-[font-1]">
-      <div className="text-[9.5vwvw] uppercase leading-[8vw]">The spark for</div>
-      <div className="text-[9.5vwvw] uppercase leading-[8vw]">all <div><Video/></div> things</div>
-      <div className="text-[9.5vwvw] uppercase leading-[8vw]">creative</div>
+      <div className="text-[9.5vw] uppercase leading-[8vw] flex justify-center items-center">The spark for</div>
+      <div className="text-[9.5vw] uppercase leading-[8vw] flex justify-center items-center">all <div className="h-[8vw] rounded-full overflow-hidden"><Video/></div> things</div>
+      <div className="text-[9.5vw] uppercase leading-[8vw] flex justify-center items-center">creative</div>
     </div> 
   );
 };
